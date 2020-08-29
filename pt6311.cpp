13a4c5a873620f9c03cd3d19eae529b27f5f8d7e
H3ndrik-Jan <46112431+H3ndrik-Jan@users.noreply.github.com>
2020-08-29 21:32:50 +0200

    Initial commit
    Added numerous extra functions, some off them customized for use with H3ndrik-Jan's VFD-module

--- pt6311.cpp
+++ pt6311.cpp
@@ -1,24 +1,24 @@
 #include "pt6311.h"
 #include <SPI.h>
 
-pt6311::pt6311(int CS_pin, int num_Of_Digit){
-  CS = CS_pin;
+pt6311::pt6311(uint8_t STB_pin, uint8_t num_Of_Digit){
+  STB = STB_pin;
   numOfDigit = num_Of_Digit;
-  pinMode(CS, OUTPUT);
-  digitalWrite(CS, HIGH);
+  pinMode(STB, OUTPUT);
+  digitalWrite(STB, HIGH);
   SPI.begin();
   SPI.setBitOrder(LSBFIRST);
   SPI.setClockDivider(SPI_CLOCK_DIV128);
   SPI.setDataMode(SPI_MODE3);
   SendByte(COMMAND_2 | NORMAL_OPERATION_MODE | INCREMENT_ADDRESS | WRITE_DATA_TO_DISPLAY_MODE);
-  digitalWrite(CS, LOW);
+  digitalWrite(STB, LOW);
   SPI.transfer(COMMAND_3);
-  for (int i = 0; i < 13; i++) {
+  for (uint8_t i = 0; i < 13; i++) {
     SPI.transfer(0);
     SPI.transfer(0);
     SPI.transfer(0);
   }
-  digitalWrite(CS, HIGH);
+  digitalWrite(STB, HIGH);
   delayMicroseconds(spi_timeout);
   switch(numOfDigit){
     case 9:
@@ -52,13 +52,13 @@ pt6311::pt6311(int CS_pin, int num_Of_Digit){
   SendByte(COMMAND_4 | DISPLAY_ON | PULSE_WIDTH14);
   displayState = DISPLAY_ON;
   displayBrighthess = PULSE_WIDTH14;
-  for(int i = 0; i < 16; i++)buf[i] = 0;
+  for(uint8_t i = 0; i < 12; i++)displayBuffer[i] = ' ';
 };
 
-void pt6311::setBrightness(int br){ 
+void pt6311::setBrightness(uint8_t br){ 
       displayBrighthess = br;
       SendByte(COMMAND_4 | displayState | displayBrighthess);
-    };
+};
 
 void pt6311::displayOn(){
   displayState = DISPLAY_ON;
@@ -70,7 +70,7 @@ void pt6311::displayOff(){
   SendByte(COMMAND_4 | displayState | displayBrighthess);
 }
 
-int pt6311::getBrightness(){ 
+uint8_t pt6311::getBrightness(){ 
   return displayBrighthess;
 }
 
@@ -78,23 +78,98 @@ bool pt6311::getState(){
   return displayState>0?true:false;
 }
 
-void pt6311::setDigit(int pos, char _data){ 
-  int data = symbolList[_data];
-  if(buf[pos] != data){
-    buf[pos] = data;
-    SendByte(COMMAND_2 | NORMAL_OPERATION_MODE | WRITE_DATA_TO_DISPLAY_MODE); //cmd2
-    digitalWrite(CS, LOW);
-    SPI.transfer(COMMAND_3 | (pos * 0x03));
-    SPI.transfer(byte(data));
-    SPI.transfer(byte(data >> 8));
-    SPI.transfer(byte(data >> 16));
-    digitalWrite(CS, HIGH);
-  }
+void pt6311::SendByte(uint8_t data) {
+  digitalWrite(STB, LOW);
+  SPI.transfer(data);
+  digitalWrite(STB, HIGH);
+  delayMicroseconds(spi_timeout);
 }
 
-void pt6311::SendByte(byte data) {
-  digitalWrite(CS, LOW);
+void pt6311::setLeds(uint8_t data){
+  digitalWrite(STB, LOW);
+  SPI.transfer(COMMAND_2 | NORMAL_OPERATION_MODE | INCREMENT_ADDRESS | WRITE_DATA_TO_LED_PORT);
   SPI.transfer(data);
-  digitalWrite(CS, HIGH);
-  delayMicroseconds(spi_timeout);
+  digitalWrite(STB, HIGH);
+}
+
+void pt6311::ledWrite(uint8_t number, uint8_t state){
+	number--;
+	uint8_t mask = 1 << number;
+	
+	ledState_ = (ledState_ & ~mask) | ((!state << number) & mask);
+	setLeds(ledState_);
+}
+
+
+void pt6311::writeChar(uint8_t pos, unsigned char data){ 
+	displayBuffer[pos] = data;
+}
+
+void pt6311::writeString(uint8_t pos, unsigned char data[]){
+	for(uint8_t i = pos; i<strlen((const char*)data)+pos; i++){
+		writeChar(i, data[i-pos]);
+	}
+}
+void pt6311::writeString(uint8_t pos, const char data[]){
+	for(uint8_t i = pos; i<strlen((const char*)data)+pos; i++){
+		writeChar(i, data[i-pos]);
+	}
+}
+
+void pt6311::setCursor(uint8_t pos){
+	digitalWrite(STB, LOW);
+	SPI.transfer(COMMAND_3 | (pos * 0x03));
+	digitalWrite(STB, HIGH);
+}
+
+void pt6311::getBytes(uint8_t *first, uint8_t *second, uint8_t pos){
+	uint8_t currentChar = displayBuffer[11-pos];
+	uint16_t data = pgm_read_word_near(alphafonttable+currentChar);
+	
+//Shuffle the bit positions to be compatible with your pinout.
+//bit permutation code generated using: http://programming.sirrida.de/calcperm.php
+data = ((data & 0x00002000) << 1)
+  | ((data & 0x00001002) << 3)
+  | ((data & 0x00000020) << 6)
+  | ((data & 0x00000008) << 9)
+  | ((data & 0x00000001) << 10)
+  | ((data & 0x00000004) << 11)
+  | ((data & 0x00000800) >> 11)
+  | ((data & 0x00008000) >> 7)
+  | ((data & 0x00004480) >> 5)
+  | ((data & 0x00000250) >> 3)
+  | ((data & 0x00000100) >> 1);
+ 
+ //Now reverse order of the bits
+ uint16_t revData = 0;
+  for(int i=0;i<16;i++)
+     if(data & (1 << i)) revData |= 1 << (15-i);
+ 
+ *first =  (uint8_t)((revData >> 8)&0xFF);
+ *second = (uint8_t)(revData & 0xFF);
+}
+
+void pt6311::print(uint8_t pos, const char data[]){
+	writeString(pos, data);
+	setCursor(pos);
+	writeLine();
+}
+
+void pt6311::writeLine(){
+	uint8_t firstByte;
+	uint8_t secondByte;
+	digitalWrite(STB, LOW);
+	SPI.transfer(0b01000000);
+	for(int i = 0; i<12; i++){
+		getBytes(&firstByte, &secondByte, i);
+		SPI.transfer(firstByte);
+		SPI.transfer(secondByte);
+		SPI.transfer(0b00000000);	//write third unused byte to increment address
+	}
+	digitalWrite(STB, HIGH);
+}
+
+void pt6311::clearBuffer(void){
+	for(uint8_t i = 0; i<12; i++)
+		displayBuffer[i] = ' ';
 }
--- pt6311.h
+++ pt6311.h
@@ -1,4 +1,8 @@
 #include <SPI.h>
+#include <avr/pgmspace.h>
+
+#include "fonts.h"
+
 #pragma once
 #define COMMAND_1 0b00000000
 #define COMMAND_2 0b01000000
@@ -38,19 +42,75 @@
 class pt6311{
   private:
     const int spi_timeout = 25;
-    int CS;
-    int numOfDigit;
-    int displayState;
-    int displayBrighthess;
-    int buf[16];
-    void SendByte(byte data);
+    uint8_t STB;
+    uint8_t numOfDigit;
+    uint8_t displayState;
+    uint8_t displayBrighthess;
+	uint8_t ledState_;
+	unsigned char displayBuffer[12];
+	
+	void setLeds(uint8_t data);
+	void SendByte(uint8_t data);
+    
   public:
-    short int symbolList[512];
-    pt6311(int CS_pin, int num_Of_Digit);
-    void setBrightness(int br); // from 0 to 7
-    void displayOn();
-    void displayOff();
-    int getBrightness(); //return brightness
+  
+    pt6311(uint8_t STB_pin, uint8_t num_Of_Digit);
+    void setBrightness(uint8_t br); // from 0 to 7
+    void displayOn(void);
+    void displayOff(void);
+    uint8_t getBrightness(); //return brightness
     bool getState(); // return on or off
-    void setDigit(int pos, char _data); // pos from 0 to numOfDigit
+	
+	void ledWrite(uint8_t number, uint8_t state);
+	void writeChar(uint8_t pos, unsigned char data);
+	void writeLine(void);
+	void getBytes(uint8_t *first, uint8_t *second, uint8_t pos);
+	void writeString(uint8_t pos, unsigned char data[]);
+	void writeString(uint8_t pos, const char data[]);
+	void setCursor(uint8_t pos);
+	void print(uint8_t pos, const char data[]);
+	void clearBuffer(void);
 };
+/*
+	Adafruit							  1  2 3 4 5 6 7 8 9  10 11 12 13 14 15 16
+	bit position -> segment map: 0 DP N M L K J H G2 G1 F  E  D  C  B  A
+	(source: https://learn.adafruit.com/14-segment-alpha-numeric-led-featherwing/usage )
+	
+	C1-1 = L
+	C1-2 = E
+	C1-3 = G2
+	C1-4 = G1
+	C1-5 = B
+	C1-6 = K
+	C1-7 = J
+	C1-8 = H
+	
+	C2-1 = NC
+	C2-2 = NC
+	C2-3 = A
+	C2-4 = F
+	C2-5 = D
+	C2-6 = C
+	C2-7 = N
+	c2-8 = M
+	
+	0-8
+	1-9
+	2-14
+	3-15
+	4-0
+	5-5
+	6-6
+	7-7
+	8-2
+	9-3
+	10-11
+	11-1
+	12-12
+	13-13
+	14-4
+	15-10
+
+//bit permutation code generated using: http://programming.sirrida.de/calcperm.php	
+8 9 14 15 0 5 6 7 2 3 11 1 12 13 4 10
+*/
